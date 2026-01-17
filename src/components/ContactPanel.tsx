a39de40fe4677c52267905af79097dea690b6229
sanduninduwara <sanduninduwara2@gmail.com>
2026-01-17 12:47:55 +0530

    remove mobile numbers

--- src/components/ContactPanel.tsx
+++ src/components/ContactPanel.tsx
@@ -1,5 +1,5 @@
 
-import { Linkedin, Mail, Phone, Github } from "lucide-react";
+import { Linkedin, Mail, Github } from "lucide-react";
 
 export function ContactPanel() {
   return (
@@ -20,13 +20,6 @@ export function ContactPanel() {
         <Mail className="w-5 h-5 text-red-600 group-hover:text-red-700 transition-colors" />
         <span className="sr-only">Email</span>
       </a>
-      {/* <a
-        href="tel:+94766763114"
-        className="group inline-flex items-center gap-1 rounded-full border border-border px-3 py-1 hover:bg-accent animate-fade-in"
-      >
-        <Phone className="w-5 h-5 text-green-700 group-hover:text-green-800 transition-colors" />
-        <span className="sr-only">Phone</span>
-      </a> */}
       <a
         href="https://github.com/sanduninduwara"
         className="group inline-flex items-center gap-1 rounded-full border border-border px-3 py-1 hover:bg-accent animate-fade-in"
--- src/components/ReferencePanel.tsx
+++ src/components/ReferencePanel.tsx
@@ -7,12 +7,12 @@ export function ReferencePanel() {
         <ReferenceCard
           name="Dilum Bandara PhD"
           desc="Senior Research Scientist, CSIRO Data61"
-          contact="dilum.bandara@csiro.au | +61 294 905 898, +61 433 915 729"
+          contact="dilum.bandara@csiro.au"
         />
         <ReferenceCard
           name="Nimsara Fernando"
           desc="Software Engineer, WSO2"
-          contact="nimsara@wso2.com | +94716180388"
+          contact="nimsara@wso2.com"
         />
       </div>
     </div>
